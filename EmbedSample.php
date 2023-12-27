1bbcfcedb0eabba006f9a963ce83af65a0c95e8b
jjamesonn <126941443+jjamesonn@users.noreply.github.com>
2023-12-27 14:57:07 -0500

    Create EmbedSample.php

---
+++ EmbedSample.php
@@ -0,0 +1,16 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>onthemon.io</title>
+    <meta name="" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+  </head>
+  <body 
+    <script src="https://staging.storage.mobiniti.com/staging/embed/v3/widget.js" data-id="djEM" data-color="003DFF" data-label="Sign Me Up" data-fields="name,birthdate" data-required="birthdate"></script>
+  </body>
+</html>
